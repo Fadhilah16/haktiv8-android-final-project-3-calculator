808b2bc2b2dfa0bfda096a2aba17dec766653cd1
Fadhilah16 <fadilahriskifatoni@gmail.com>
2022-12-03 16:29:22 +0700

    calculator

---
+++ app/src/main/java/com/hacktiv/calculator/InfoActivity.java
@@ -0,0 +1,26 @@
+package com.hacktiv.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class InfoActivity extends AppCompatActivity {
+
+    private ImageButton backBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        backBtn = findViewById(R.id.back_btn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+}
